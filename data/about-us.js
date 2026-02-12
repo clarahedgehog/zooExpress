73db2a2eafe2d15f8d21f791018196e3cc12ce98
Oscar Öjling <oscarojling@gmail.com>
2026-02-13 00:25:12 +0100

    Added about us page

---
+++ data/about-us.js
@@ -0,0 +1,22 @@
+export const aboutUs = [
+  {
+    name: "Clara",
+    role: "Frontend Developer",
+    bio: "lorem ipsum"
+  },
+  {
+    name: "Nathalie",
+    role: "Frontend Developer",
+    bio: "lorem ipsum"
+  },
+  {
+    name: "Oscar",
+    role: "Frontend Developer",
+    bio: "lorem ipsum"
+  },
+  {
+    name: "Emelie",
+    role: "Frontend Developer",
+    bio: "lorem ipsum"
+  }
+]
